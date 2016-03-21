64279ad82bcd846947b414a2aae1c83abf657920
ShaneXie <shane369@gmail.com>
2016-03-20 21:54:02 -0500

    modal up

--- app/js/content.js
+++ app/js/content.js
@@ -4,14 +4,32 @@
 
 	// document ready
 	$(function() {
-		alert("run on index???");
-		var trigger = '<li class="fl bdtv-trigger">'+
-			'<a href="#">鱼刺</a>'+
-			'</li>';
-		if ($(".header_nav").length) 
-			$(".header_nav").append(trigger);
-		else 
-			$(".head-nav").append(trigger);
+		initComponent.then(function () {
+			$("#test").on('click',function () {
+				$("#wrap").css("background-color","grey");
+			});
+		});
 	});
+
+	var initComponent = new Promise (
+		function (resolve, reject) {
+			var modal = '<div id="modalWrap" style="display:none;"></div>';
+			var modalURL = chrome.extension.getURL('../html/modal.html');
+
+			$("body").append(modal);
+			$( "#modalWrap" ).load( modalURL );
+
+			var trigger = '<li class="fl bdtv-trigger">'+
+				'<a href="#modal" rel="modal:open">鱼刺</a>'+
+				'</li>';
+
+			if ($(".header_nav").length) 
+				$(".header_nav").append(trigger);
+			else 
+				$(".head-nav").append(trigger);
+
+			resolve("Component initialized");
+		}
+	); 
 
 })();
